2f462370b350812dd8fc930535a5a6a59ce0914a
Keith Conger <keith.conger@gmail.com>
2024-08-18 07:16:31 -0600

    Fix formatting

--- util.c
+++ util.c
@@ -185,10 +185,10 @@ char *dbcc_basename(char *s)
 	assert(s);
 	size_t i;
 	if (!s || !*s) return ".";
-	i = strlen(s) - 1;
-    for (; i && (s[i] == '/' || s[i] == '\\'); i--) s[i] = 0;
-    for (; i && s[i-1] != '/' && s[i-1] != '\\'; i--);
-    return s + i;
+	i = strlen(s)-1;
+	for (; i&&(s[i]=='/'||s[i]=='\\'); i--) s[i] = 0;
+	for (; i&&s[i-1]!='/'&&s[i-1]!='\\'; i--);
+	return s + i;
 }
 
 
